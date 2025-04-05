2fe1af226cea9da3dad434e2f68523eb1b2c149c
2scent <ad7549@gmail.com>
2025-04-05 14:10:17 +0900

    feat: 기본 너비 및 폰트 설정

--- src/app/providers/index.tsx
+++ src/app/providers/index.tsx
@@ -1,5 +1,15 @@
 import type { AppProps } from 'next/app';
+import { Geist } from 'next/font/google';
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={`${geistSans.variable} mx-auto min-h-screen max-w-[480px] shadow-md`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
